a0835148c821cf4f600362a00fea9201857cd1a4
Joanne <jl7105@nyu.edu>
2022-08-19 13:51:25 -0400

    fixed file name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux'
-import { store } from '../../src/app/store'
+import { store } from '../app/store'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
