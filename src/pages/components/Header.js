7e5cd5f213774ec21024f21958a660efd197f271
Joanne <jl7105@nyu.edu>
2022-08-21 15:29:09 -0400

    Bottom Nav

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -63,11 +63,21 @@ function Header() {
             </div>
 
             {/* BOTTOM NAV */}
-            <div className='flex items-center bg-amazon_blue-light'>
-                <p>
+            <div className='flex items-center space-x-3 p-2 pl-6  bg-amazon_blue-light text-white text-sm'>
+                <p className='link flex items-center'>
                     <MenuIcon className='h-6 mr-1' />
                     All
                 </p>
+                <p className='link'>Prime Video</p>
+                <p className='link'>Amazon Business</p>
+                <p className='link'>Today's Deals</p>
+                {/* By default hidden on mobile, but shows up on large screen by inline-flex */}
+                <p className='link hidden lg:inline-flex'>Electronics</p>
+                <p className='link hidden lg:inline-flex'>Food & Grocery</p>
+                <p className='link hidden lg:inline-flex'>Prime</p>
+                <p className='link hidden lg:inline-flex'>Buy Again</p>
+                <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
+                <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
             </div>
         </header>
     )
