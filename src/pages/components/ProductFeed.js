82dd1306c4988df6e0aa3787eab11c87f0c6c728
Joanne <jl7105@nyu.edu>
2022-08-23 01:56:47 -0400

    ProductFeed Component

---
+++ src/pages/components/ProductFeed.js
@@ -0,0 +1,107 @@
+import React from 'react'
+// The reuseable component:
+import Product from './Product'
+
+// 'props' is usually passed through the component function
+// But, if you deconstruct 'props', you get 'products'
+// You can skip:
+// const products = props.products
+function ProductFeed({ products }) {
+    return (
+        // Deconstructing each product element
+        // Fyi - 'row-dense' - use up all the splace
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+
+            {/* First 4 products */}
+            {products
+                .slice(0, 4)
+                .map(({ id, title, price, category, description, image }) => {
+                    return (
+                        <Product
+                            key={id}
+                            title={title}
+                            price={price}
+                            category={category}
+                            description={description}
+                            image={image}
+                        />
+                    )
+                })
+            }
+
+            {/* Amazon Ad - when reach medium screen, the ad shall take the full width of the screen */}
+            <img
+                className='md:col-span-full'
+                src="https://links.papareact.com/dyz"
+                alt=""
+            />
+
+            {/* When medium screen, this product shall take two columns: */}
+            <div className='md:col-span-2'>
+                {products
+                    .slice(4, 5)
+                    .map(({ id, title, price, category, description, image }) => {
+                        return (
+                            <Product
+                                key={id}
+                                title={title}
+                                price={price}
+                                category={category}
+                                description={description}
+                                image={image}
+                            />
+                        )
+                    })
+                }
+            </div>
+
+            {/* Rest of the products */}
+            {products
+                .slice(5, products.length)
+                .map(({ id, title, price, category, description, image }) => {
+                    return (
+                        <Product
+                            key={id}
+                            title={title}
+                            price={price}
+                            category={category}
+                            description={description}
+                            image={image}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default ProductFeed
+
+
+
+
+
+/*
+props: { 
+    // This is the output from FakeStoreAPI set as value to props
+    products: [
+        {
+                    id:1,
+                    title:'...',
+                    price:'...',
+                    category:'...',
+                    description:'...',
+                    image:'...'
+                },
+                ...
+                {
+                    id:30,
+                    title:'...',
+                    price:'...',
+                    category:'...',
+                    description:'...',
+                    image:'...'
+                }
+    ]
+}
+*/
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import ProductFeed from "./components/ProductFeed";
 
-export default function Home() {
+// Where is products being deconstructed from?
+// 'props' is usually passed through the component function
+// But, if you deconstruct 'props', you get 'products'
+export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -15,7 +19,50 @@ export default function Home() {
         <Banner></Banner>
 
         {/* ProductFeed */}
+        <ProductFeed products={products}></ProductFeed>
       </main>
     </div>
   );
 }
+
+/*
+ServerSide Render:
+Just having this function, tell Nextjs that this is no longer a static page. There will be a middle server step. FIRST, render everything on the page and THEN send it to the user, rather then sending the site.
+
+GET >>> https://fakestoreapi.com/products
+*/
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products').then(res => res.json());
+  
+  // After fetching the products which is an array of data, simply return it as props:
+  return {
+    props: {
+      products,
+    }
+  }
+}
+
+/*
+props: { 
+    // This is the output from FakeStoreAPI set as value to props
+    products: [
+        {
+                    id:1,
+                    title:'...',
+                    price:'...',
+                    category:'...',
+                    description:'...',
+                    image:'...'
+                },
+                ...
+                {
+                    id:30,
+                    title:'...',
+                    price:'...',
+                    category:'...',
+                    description:'...',
+                    image:'...'
+                }
+    ]
+}
+*/
